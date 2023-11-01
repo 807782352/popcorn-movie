de6963bcbb989a0ebb2d0f5403ac90521b2cdb40
z297xu <kyrie807782352@gmail.com>
2023-11-01 14:17:24 -0400

    project initialization & navbar split

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>Popcorn Movies</h1>
+    </div>
+  );
+}
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "../Search";
+import Logo from "../Logo";
+import NumResults from "../NumResults";
+
+export default function NavBar() {
+
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  );
+}
