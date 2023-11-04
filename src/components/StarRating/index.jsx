56a722ee6a391084e6f8d06dc8ab3a9873da398f
z297xu <kyrie807782352@gmail.com>
2023-11-03 23:29:51 -0400

    test star rating systems

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -2,17 +2,42 @@ import React, { useState } from "react";
 import "./index.css";
 import Star from "../Star";
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+/**
+ *
+ * @param {onSetRating} 外部接口（方法），可以把内部的rating值传递出去
+ * @returns
+ */
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating = null,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   return (
-    <div className="container">
+    <div className={`container ${className}`}>
       <div className="stars">
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => {
+              setRating(i + 1);
+              if (onSetRating) {
+                onSetRating(i + 1);
+              }
+            }}
             full={tempRating ? i + 1 <= tempRating : i + 1 <= rating}
             onHoverIn={() => {
               setTempRating(i + 1);
@@ -20,10 +45,16 @@ export default function StarRating({ maxRating = 5 }) {
             onHoverOut={() => {
               setTempRating(0);
             }}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p className="text">{tempRating || (rating || "")}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 // import "./index.css"
@@ -8,8 +8,25 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
-    <StarRating />
+    <Test />
+    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
   </StrictMode>
 );
+
+function Test() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        onSetRating={setRating}
+      />
+      <p>The rating is {rating}</p>
+    </div>
+  );
+}
